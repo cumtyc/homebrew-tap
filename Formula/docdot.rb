ff976bee6eaa99a7c617f89c9e823325fbb0bf84
yangcheng <yc.cumt@qq.com>
2026-09-14 15:42:48 +0800

    fix(docdot): defer converter installation

--- Formula/docdot.rb
+++ Formula/docdot.rb
@@ -13,19 +13,9 @@ class Docdot < Formula
   depends_on arch: :arm64
   depends_on :macos
 
-  resource "docdot-converter" do
-    url "https://dl.docdot.ai/releases/docdot-converter/0.1.4/docdot-converter-darwin-arm64", using: :nounzip
-    sha256 "362e6d7990f7fc613f9d16951b295f443a5cd6ade5e93bf54cf4f4abcf60ab8f"
-  end
-
   def install
     bin.install "docdot-darwin-arm64" => "docdot"
     (bin/"docdot").chmod 0755
-
-    resource("docdot-converter").stage do
-      bin.install "docdot-converter-darwin-arm64" => "docdot-converter"
-      (bin/"docdot-converter").chmod 0755
-    end
   end
 
   def caveats
@@ -37,6 +27,5 @@ class Docdot < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/docdot --version")
-    assert_match version.to_s, shell_output("#{bin}/docdot-converter --version")
   end
 end
